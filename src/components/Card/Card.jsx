8735599d28a184431959c70b0e48827756a6a297
Ankit Joshi <67504902+ankit9015@users.noreply.github.com>
2022-03-28 17:30:41 +0530

    refactor: Card refactored

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom"
 
 const CardButton = (props) => {
-  return 
+  return (
     <Link
       to={props.link}
       className={`card-btn link-btn ${
@@ -10,7 +10,11 @@ const CardButton = (props) => {
       }`}
     >
       {props.info}
-    </Link>
+    </Link>)
+};
+
+const CardImage = (props) => {
+  return <img className="card-img" src={props.src} alt={props.alt} />;
 };
 
 const Card = (props) => {
@@ -21,17 +25,9 @@ const Card = (props) => {
         } ${props.overlayText ? "text-overlay-container" : ''}
         ${props.className? props.className : ""}`}
       >
-        
-        <img
-          className="card-img"
-          src={props.img.src}
-          alt={props.img.alt}
-        />
 
-        {/* this comment is for future PR, please ignore */}
-
-        {/* <CardImage src={props.img.src} 
-          alt={props.img.alt}/> */}
+        <CardImage src={props.img.src} 
+          alt={props.img.alt}/>
         
         
         <div className="card-body">
@@ -40,17 +36,8 @@ const Card = (props) => {
             {props.description && <p className="card-description">{props.description}</p>}
 
             {props.cardButton &&  
-            <Link
-      to={props.cardButton.link}
-      className={`card-btn link-btn ${
-        props.cardButton.variant === "primary" ? "button-primary" : "button-secondary"
-      }`}
-    >
-      {props.cardButton.info}
-    </Link>}
-
-    {/* this comment is for future PR, please ignore */}
-    {/* <CardButton link={props.cardButton.link} info={props.cardButton.info} variant={props.cardButton.variant}/> */}
+            <CardButton link={props.cardButton.link} info={props.cardButton.info} variant={props.cardButton.variant}/>
+            }
 
         </div>
         {props.overlayText && 
@@ -59,22 +46,7 @@ const Card = (props) => {
   );
 }
 
-const CardImage = (props) => {
-  return <img className="card-img" src={props.src} alt={prop.alt} />;
-};
-
-const CardBody = (props) => {
-  return (
-    
-      <div className="card-body">
-            {props.title && <h3 className="card-title">{props.title}</h3>}
-            {props.subtitle && <h6 className="card-title-sub ">{props.subtitle}</h6>}
-            {props.description && <p className="card-description">{props.description}</p>}
-            </div>
-
-  );
-};
-
 
 
 export default Card;
+export {Card, CardImage, CardButton};
