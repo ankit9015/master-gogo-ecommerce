b437980a4ef68660a5f68bd9983e331658a0412a
Ankit Joshi <67504902+ankit9015@users.noreply.github.com>
2022-04-02 04:37:19 +0530

    feat: signup-static-ui

--- src/pages/Auth/Signup/Signup.jsx
+++ src/pages/Auth/Signup/Signup.jsx
@@ -1,7 +1,108 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-function Signup() {
-  return <div>Signup</div>;
+import "../auth.css";
+
+function Signup({ signupInfo }) {
+  const [signupForm, setSignupForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    acceptTAndC: false,
+  });
+
+  useEffect(() => console.log(signupForm));
+
+  const updateSignupForm = (e) => {
+    e.target.name === "acceptTAndC"
+      ? setSignupForm({
+          ...signupForm,
+          rememberUser: !signupForm.acceptTAndC,
+        })
+      : setSignupForm({
+          ...signupForm,
+          [e.target.name]: e.target.value,
+        });
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="flex-row flex-center login-main">
+      <div className="card-vertical form-card card-shadow ">
+        <div className="card-body text-md">
+          <h1 className="card-title text-lg m-m font-extrabold text-center">
+            Signup
+          </h1>
+          <div className="form-container ">
+            <label className="flex-column">
+              <span className="text-md socketui-label label-required">
+                Email:
+              </span>
+              <input
+                className="socketui-input text-md"
+                type="email"
+                name="email"
+                placeholder="xyz@abc.com"
+                required
+                value={signupForm.email}
+                onChange={(e) => updateSignupForm(e)}
+              />
+            </label>
+            <label className="flex-column">
+              <span className="text-md socketui-label label-required">
+                Password:
+              </span>
+              <div className="flex-row input-border">
+                <input
+                  className="socketui-input password-input text-md"
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  placeholder="******"
+                  required
+                  value={signupForm.password}
+                  onChange={(e) => updateSignupForm(e)}
+                ></input>
+                <span
+                  className="text-blue password-show"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                >
+                  show
+                </span>
+              </div>
+            </label>
+
+            <label>
+              <input
+                type="checkbox"
+                name="rememberUser"
+                value={signupForm.acceptTAndC}
+                onChange={(e) => updateSignupForm(e)}
+              />
+              <span className="text-md">
+                I accept all the Terms and Conditions
+              </span>
+            </label>
+
+            <Link
+              to="../Products"
+              className="button-primary link-btn text-md text-center"
+              onClick={() => signupInfo.setIsSignup(true)}
+            >
+              Create new account
+            </Link>
+
+            <Link
+              className="text-center link-btn button-outline-secondary"
+              to="../Login"
+            >
+              Already have an account
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
